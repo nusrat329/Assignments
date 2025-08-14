7a3e4b67d16edb405054fc1ae952071bb0ddd264
nusrat329 <audrinusrat@gmail.com>
2025-08-14 12:28:08 -0700

    Add files via upload

---
+++ generate multiplication table.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number to generate multiplication table: ");
+    scanf("%d", &num);
+    printf("Multiplication Table of %d:\n", num);
+    for (int i = 1; i <= 10; ++i) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
+}
