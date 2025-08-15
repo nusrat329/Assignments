75e23e9a69cfbb1baf5ec872c4a56779465c0d35
nusrat329 <audrinusrat@gmail.com>
2025-08-15 07:39:03 -0700

    Add files via upload

---
+++ sort a string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+void sortString(char str[]) {
+    int n = strlen(str);
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (str[j] > str[j + 1]) {
+                char temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+    sortString(str);
+    printf("Sorted string: %s\n", str);
+
+    return 0;
+}
