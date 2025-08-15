3c59e172cd7cd3d4ed0b1e4608419eccd5fec206
nusrat329 <audrinusrat@gmail.com>
2025-08-15 00:50:04 -0700

    Add files via upload

---
+++ print inverted pyramid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int i, j, space, rows;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = rows; i >= 1; i--) {
+        for (space = 0; space < rows - i; space++) {
+            printf(" ");
+        }
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
